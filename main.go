0168d16a58a901beb3c5b88820deeae058cc6e65
“lijiesheng” <ljs_hsm@163.com>
2023-01-18 22:24:26 +0800

    引入css文件

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 	// template/* 意思是找当前项目路径下template文件夹下所有的html文件
 	r.LoadHTMLGlob("template/**/*") // 所有的 html 文件都是 /**/xx.html
 
+	// 这个 /static 对应的 <link rel="stylesheet" href="/static/css/index.css"> 中的 /static
+	// root 的 static 是代表到 static 文件中去找
+	r.Static("/static", "static")
+
 	r.Run(":8083")
 }
 
